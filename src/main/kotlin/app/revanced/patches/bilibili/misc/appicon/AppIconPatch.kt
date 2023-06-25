f4886ff9c4d49a708885cbcdb6f1e51ebd65718f
Kofua <1638183271zjn@gmail.com>
2023-06-25 17:27:27 +0800

    feat（bilibili): restore app icon

---
+++ src/main/kotlin/app/revanced/patches/bilibili/misc/appicon/AppIconPatch.kt
@@ -0,0 +1,41 @@
+package app.revanced.patches.bilibili.misc.appicon
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.bilibili.annotations.BiliBiliCompatibility
+import app.revanced.util.resources.ResourceUtils
+import app.revanced.util.resources.ResourceUtils.copyResources
+
+@Patch
+@BiliBiliCompatibility
+@Name("app-icon")
+@Description("恢复APP图标")
+class AppIconPatch : ResourcePatch {
+    override fun execute(context: ResourceContext): PatchResult {
+        val iconFiles = arrayOf(
+            "ic_launcher.png",
+            "ic_launcher_background.png",
+            "ic_launcher_foreground.png",
+            "ic_launcher_foreground_round.png",
+            "ic_launcher_round.png"
+        )
+        val iconPaths = arrayOf(
+            "mipmap-mdpi",
+            "mipmap-hdpi",
+            "mipmap-xhdpi",
+            "mipmap-xxhdpi",
+            "mipmap-xxxhdpi"
+        )
+        iconPaths.forEach {
+            ResourceUtils.ResourceGroup(it, *iconFiles).run {
+                context.copyResources("bilibili/appicon", this)
+            }
+        }
+        return PatchResultSuccess()
+    }
+}
