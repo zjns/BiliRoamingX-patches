3cf6031ca9e3cd117aef3fcb0ab04ff99e569428
Kofua <1638183271zjn@gmail.com>
2023-06-26 16:11:09 +0800

    feat(bilibili): about

--- src/main/kotlin/app/revanced/patches/bilibili/misc/settings/patch/SettingsResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/bilibili/misc/settings/patch/SettingsResourcePatch.kt
@@ -49,6 +49,7 @@ class SettingsResourcePatch : AbstractSettingsResourcePatch(
         "biliroaming_setting_space.xml",
         "biliroaming_setting_customize_search.xml",
         "biliroaming_setting_filter_search_type.xml",
+        "biliroaming_setting_about.xml"
     )
     private val layouts = arrayOf(
         "biliroaming_dialog_argb_color_choose.xml",
