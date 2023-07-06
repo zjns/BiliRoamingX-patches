18955c103919425f8fcfb61a04394feff9c3db9c
Kofua <1638183271zjn@gmail.com>
2023-07-06 11:35:33 +0800

    feat(bilibili): better teenager mode patch

--- src/main/kotlin/app/revanced/patches/bilibili/misc/other/patch/TeenagerModePatch.kt
+++ src/main/kotlin/app/revanced/patches/bilibili/misc/other/patch/TeenagerModePatch.kt
@@ -9,27 +9,34 @@ import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
 import app.revanced.patches.bilibili.annotations.BiliBiliCompatibility
+import app.revanced.patches.bilibili.misc.other.fingerprints.TeenagerModeCheckFingerprint
+import app.revanced.patches.bilibili.misc.other.fingerprints.TeenagerModeOnShowFingerprint
 
 @Patch
 @BiliBiliCompatibility
 @Name("disable-teenager-mode-dialog")
 @Description("禁用青少年模式弹窗")
 @Version("0.0.1")
-class TeenagerModePatch : BytecodePatch() {
+class TeenagerModePatch : BytecodePatch(
+    listOf(TeenagerModeCheckFingerprint, TeenagerModeOnShowFingerprint)
+) {
     override fun execute(context: BytecodeContext): PatchResult {
-        context.findClass("Lcom/bilibili/teenagersmode/ui/TeenagersModeDialogActivity;")?.let { clazz ->
-            clazz.mutableClass.methods.find { it.name == "onCreate" }?.addInstructionsWithLabels(
-                1, """
-                    invoke-static {p0}, Lapp/revanced/bilibili/patches/TeenagerModePatch;->disableDialog(Landroid/app/Activity;)Z
-                    move-result v0
-                    if-eqz v0, :jump
-                    return-void
-                    :jump
-                    nop
+        fun MutableMethod.disable() {
+            addInstructionsWithLabels(
+                0, """
+                invoke-static {}, Lapp/revanced/bilibili/patches/TeenagerModePatch;->shouldDisable()Z
+                move-result v0
+                if-eqz v0, :jump
+                return-void
+                :jump
+                nop
             """.trimIndent()
             )
         }
+        TeenagerModeCheckFingerprint.result?.mutableMethod?.disable()
+        TeenagerModeOnShowFingerprint.result?.mutableMethod?.disable()
         return PatchResultSuccess()
     }
 }
