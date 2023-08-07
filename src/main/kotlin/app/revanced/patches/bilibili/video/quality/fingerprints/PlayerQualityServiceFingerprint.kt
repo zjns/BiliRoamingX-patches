b3de26c082fc2446845e21c77299439c68003f3b
Kofua <1638183271zjn@gmail.com>
2023-08-07 10:50:32 +0800

    fix(bilibili): custom half screen default quality

--- src/main/kotlin/app/revanced/patches/bilibili/video/quality/fingerprints/PlayerQualityServiceFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/bilibili/video/quality/fingerprints/PlayerQualityServiceFingerprint.kt
@@ -1,8 +1,8 @@
 package app.revanced.patches.bilibili.video.quality.fingerprints
 
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patches.bilibili.patcher.fingerprint.MultiMethodFingerprint
 
-object PlayerQualityServiceFingerprint : MethodFingerprint(
+object PlayerQualityServiceFingerprint : MultiMethodFingerprint(
     returnType = "I",
     strings = listOf("player.unite_login_qn", "player.unite_unlogin_qn")
 )
--- src/main/kotlin/app/revanced/patches/bilibili/video/quality/patch/VideoQualityPatch.kt
+++ src/main/kotlin/app/revanced/patches/bilibili/video/quality/patch/VideoQualityPatch.kt
@@ -7,11 +7,12 @@ import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
-import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.bilibili.annotations.BiliBiliCompatibility
+import app.revanced.patches.bilibili.patcher.patch.MultiMethodBytecodePatch
+import app.revanced.patches.bilibili.utils.toErrorResult
 import app.revanced.patches.bilibili.video.quality.fingerprints.PlayerQualityServiceFingerprint
 import app.revanced.patches.bilibili.video.quality.fingerprints.PlayerSettingHelperFingerprint
 import org.jf.dexlib2.iface.Method
@@ -21,43 +22,46 @@ import org.jf.dexlib2.iface.Method
 @BiliBiliCompatibility
 @Description("视频默认画质设置")
 @Version("0.0.1")
-class VideoQualityPatch : BytecodePatch(
-    listOf(
-        PlayerSettingHelperFingerprint,
-        PlayerQualityServiceFingerprint,
-    )
+class VideoQualityPatch : MultiMethodBytecodePatch(
+    fingerprints = listOf(PlayerSettingHelperFingerprint),
+    multiFingerprints = listOf(PlayerQualityServiceFingerprint)
 ) {
     override fun execute(context: BytecodeContext): PatchResult {
+        super.execute(context)
         var defaultQnMethod: Method? = null
         PlayerSettingHelperFingerprint.result?.also {
             defaultQnMethod = it.method
         }?.mutableMethod?.addInstructionsWithLabels(
             0, """
-                    invoke-static {}, Lapp/revanced/bilibili/patches/VideoQualityPatch;->fullScreenQuality()I
-                    move-result v0
-                    if-eqz v0, :jump
-                    return v0
-                    :jump
-                    nop
-                """.trimIndent()
-        ) ?: return PlayerSettingHelperFingerprint.toErrorResult()
-        PlayerQualityServiceFingerprint.result?.mutableMethod?.addInstructionsWithLabels(
-            0, """
-                    invoke-static {}, Lapp/revanced/bilibili/patches/VideoQualityPatch;->getMatchedHalfScreenQuality()I
-                    move-result v0
-                    if-eqz v0, :jump
-                    return v0
-                    :jump
-                    nop
+            invoke-static {}, Lapp/revanced/bilibili/patches/VideoQualityPatch;->fullScreenQuality()I
+            move-result v0
+            if-eqz v0, :jump
+            return v0
+            :jump
+            nop
         """.trimIndent()
-        ) ?: return PlayerQualityServiceFingerprint.toErrorResult()
+        ) ?: return PlayerSettingHelperFingerprint.toErrorResult()
+        PlayerQualityServiceFingerprint.result.ifEmpty {
+            return PlayerQualityServiceFingerprint.toErrorResult()
+        }.forEach {
+            it.mutableMethod.addInstructionsWithLabels(
+                0, """
+                invoke-static {}, Lapp/revanced/bilibili/patches/VideoQualityPatch;->getMatchedHalfScreenQuality()I
+                move-result v0
+                if-eqz v0, :jump
+                return v0
+                :jump
+                nop
+            """.trimIndent()
+            )
+        }
         context.findClass("Lapp/revanced/bilibili/patches/VideoQualityPatch;")?.let { clazz ->
             clazz.mutableClass.methods.find { it.name == "defaultQn" }?.addInstructions(
                 0, """
                 invoke-static {}, $defaultQnMethod
                 move-result v0
                 return v0
-                """.trimIndent()
+            """.trimIndent()
             )
         }
         return PatchResultSuccess()
