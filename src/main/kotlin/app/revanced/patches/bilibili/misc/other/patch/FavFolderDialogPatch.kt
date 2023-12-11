4dae70394a2c175c880e020db7ee1b80550c1612
Kofua <1638183271zjn@gmail.com>
2023-12-11 15:47:53 +0800

    fix(bilibili): disable auto subscribe

--- src/main/kotlin/app/revanced/patches/bilibili/misc/other/patch/FavFolderDialogPatch.kt
+++ src/main/kotlin/app/revanced/patches/bilibili/misc/other/patch/FavFolderDialogPatch.kt
@@ -7,6 +7,7 @@ import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.bilibili.misc.other.fingerprints.FavFolderOnDataSuccessFingerprint
+import app.revanced.patches.bilibili.misc.other.fingerprints.FavFolderOnDataSuccessNewFingerprint
 import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction35c
 
@@ -15,21 +16,28 @@ import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction35c
     description = "禁止自动勾选订阅合集",
     compatiblePackages = [CompatiblePackage(name = "tv.danmaku.bili"), CompatiblePackage(name = "tv.danmaku.bilibilihd")]
 )
-object FavFolderDialogPatch : BytecodePatch(setOf(FavFolderOnDataSuccessFingerprint)) {
+object FavFolderDialogPatch : BytecodePatch(
+    setOf(
+        FavFolderOnDataSuccessFingerprint,
+        FavFolderOnDataSuccessNewFingerprint
+    )
+) {
     override fun execute(context: BytecodeContext) {
-        FavFolderOnDataSuccessFingerprint.result?.mutableMethod?.run {
-            val (register, insertIndex) = implementation!!.instructions.withIndex()
-                .firstNotNullOfOrNull { (index, inst) ->
-                    if (inst.opcode == Opcode.INVOKE_VIRTUAL && inst is BuilderInstruction35c
-                        && inst.reference.toString() == "Landroid/widget/CheckBox;->setChecked(Z)V"
-                    ) (inst.registerD to index) else null
-                } ?: throw FavFolderOnDataSuccessFingerprint.exception
-            addInstructions(
-                insertIndex, """
-                invoke-static {v$register}, Lapp/revanced/bilibili/patches/FavFolderDialogPatch;->shouldChecked(Z)Z
-                move-result v$register
-            """.trimIndent()
-            )
-        } ?: throw FavFolderOnDataSuccessFingerprint.exception
+        arrayOf(FavFolderOnDataSuccessFingerprint, FavFolderOnDataSuccessNewFingerprint).forEach {
+            it.result?.mutableMethod?.run {
+                val (register, insertIndex) = implementation!!.instructions.withIndex()
+                    .firstNotNullOfOrNull { (index, inst) ->
+                        if (inst.opcode == Opcode.INVOKE_VIRTUAL && inst is BuilderInstruction35c
+                            && inst.reference.toString() == "Landroid/widget/CheckBox;->setChecked(Z)V"
+                        ) (inst.registerD to index) else null
+                    } ?: throw it.exception
+                addInstructions(
+                    insertIndex, """
+                    invoke-static {v$register}, Lapp/revanced/bilibili/patches/FavFolderDialogPatch;->shouldChecked(Z)Z
+                    move-result v$register
+                """.trimIndent()
+                )
+            } ?: throw it.exception
+        }
     }
 }
