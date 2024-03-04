f713c2b4f0f345a816154f1056066a345bfa05cb
Kofua <1638183271zjn@gmail.com>
2024-03-04 22:08:26 +0800

    refactor(bilibili): customize video quality

--- src/main/kotlin/app/revanced/patches/bilibili/video/quality/fingerprints/PlayerQualityServiceFingerprint.kt
+++
@@ -1,8 +0,0 @@
-package app.revanced.patches.bilibili.video.quality.fingerprints
-
-import app.revanced.patches.bilibili.patcher.fingerprint.MultiMethodFingerprint
-
-object PlayerQualityServiceFingerprint : MultiMethodFingerprint(
-    returnType = "I",
-    strings = listOf("player.unite_login_qn", "player.unite_unlogin_qn")
-)
--- src/main/kotlin/app/revanced/patches/bilibili/video/quality/patch/VideoQualityPatch.kt
+++ src/main/kotlin/app/revanced/patches/bilibili/video/quality/patch/VideoQualityPatch.kt
@@ -1,15 +1,13 @@
 package app.revanced.patches.bilibili.video.quality.patch
 
-import app.revanced.util.exception
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
+import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.bilibili.patcher.patch.MultiMethodBytecodePatch
-import app.revanced.patches.bilibili.utils.exception
-import app.revanced.patches.bilibili.video.quality.fingerprints.PlayerQualityServiceFingerprint
 import app.revanced.patches.bilibili.video.quality.fingerprints.PlayerSettingHelperFingerprint
+import app.revanced.util.exception
 import com.android.tools.smali.dexlib2.iface.Method
 
 @Patch(
@@ -21,12 +19,8 @@ import com.android.tools.smali.dexlib2.iface.Method
         CompatiblePackage(name = "com.bilibili.app.in")
     ]
 )
-object VideoQualityPatch : MultiMethodBytecodePatch(
-    fingerprints = setOf(PlayerSettingHelperFingerprint),
-    multiFingerprints = setOf(PlayerQualityServiceFingerprint)
-) {
+object VideoQualityPatch : BytecodePatch(setOf(PlayerSettingHelperFingerprint)) {
     override fun execute(context: BytecodeContext) {
-        super.execute(context)
         var defaultQnMethod: Method? = null
         PlayerSettingHelperFingerprint.result?.also {
             defaultQnMethod = it.method
@@ -40,28 +34,13 @@ object VideoQualityPatch : MultiMethodBytecodePatch(
             nop
         """.trimIndent()
         ) ?: throw PlayerSettingHelperFingerprint.exception
-        PlayerQualityServiceFingerprint.result.ifEmpty {
-            throw PlayerQualityServiceFingerprint.exception
-        }.forEach {
-            it.mutableMethod.addInstructionsWithLabels(
-                0, """
-                invoke-static {}, Lapp/revanced/bilibili/patches/VideoQualityPatch;->getMatchedHalfScreenQuality()I
-                move-result v0
-                if-eqz v0, :jump
-                return v0
-                :jump
-                nop
-            """.trimIndent()
-            )
-        }
-        context.findClass("Lapp/revanced/bilibili/patches/VideoQualityPatch;")?.let { clazz ->
-            clazz.mutableClass.methods.find { it.name == "defaultQn" }?.addInstructions(
+        context.findClass("Lapp/revanced/bilibili/patches/VideoQualityPatch;")!!
+            .mutableClass.methods.first { it.name == "defaultQn" }.addInstructions(
                 0, """
                 invoke-static {}, $defaultQnMethod
                 move-result v0
                 return v0
             """.trimIndent()
             )
-        }
     }
 }
