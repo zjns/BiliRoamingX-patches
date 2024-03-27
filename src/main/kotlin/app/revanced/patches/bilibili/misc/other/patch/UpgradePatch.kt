38ad98a44f1c77e8c535dddcac72bb7c36bdf80f
Kofua <1638183271zjn@gmail.com>
2024-03-27 12:17:33 +0800

    feat(bilibili): support check update in module settings

--- src/main/kotlin/app/revanced/patches/bilibili/misc/other/patch/UpgradePatch.kt
+++ src/main/kotlin/app/revanced/patches/bilibili/misc/other/patch/UpgradePatch.kt
@@ -1,12 +1,15 @@
 package app.revanced.patches.bilibili.misc.other.patch
 
 import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.bilibili.misc.other.fingerprints.AttachChannelInfoFingerprint
+import app.revanced.patches.bilibili.misc.other.fingerprints.MainCommonServiceImplFingerprint
+import app.revanced.patches.bilibili.utils.className
 import app.revanced.util.exception
+import com.android.tools.smali.dexlib2.Opcode
 
 @Patch(
     name = "Upgrade",
@@ -17,17 +20,30 @@ import app.revanced.util.exception
         CompatiblePackage(name = "com.bilibili.app.in")
     ]
 )
-object UpgradePatch : BytecodePatch(setOf(AttachChannelInfoFingerprint)) {
+object UpgradePatch : BytecodePatch(setOf(AttachChannelInfoFingerprint, MainCommonServiceImplFingerprint)) {
     override fun execute(context: BytecodeContext) {
-        AttachChannelInfoFingerprint.result?.mutableMethod?.addInstructionsWithLabels(
-            0, """
-            invoke-static {}, Lapp/revanced/bilibili/patches/UpgradePatch;->customUpdate()Z
-            move-result v0
-            if-eqz v0, :jump
-            return-void
-            :jump
-            nop
-        """.trimIndent()
+        AttachChannelInfoFingerprint.result?.mutableMethod?.addInstructions(
+            0, "return-void"
         ) ?: throw AttachChannelInfoFingerprint.exception
+        MainCommonServiceImplFingerprint.result?.classDef?.methods?.find {
+            it.returnType == "V" && it.parameterTypes == listOf("Landroid/app/Activity;")
+        }?.let { m ->
+            context.findClass(
+                "Lapp/revanced/bilibili/settings/fragments/AboutFragment;"
+            )!!.mutableClass.run {
+                methods.first { it.name == "<init>" }.run {
+                    val returnIndex = implementation!!.instructions.indexOfFirst {
+                        it.opcode == Opcode.RETURN_VOID
+                    }
+                    val field = fields.first { it.name == "checkUpdateMethod" }
+                    addInstructions(
+                        returnIndex, """
+                        const-string v0, "${m.definingClass.className}#${m.name}"
+                        iput-object v0, p0, $field
+                    """.trimIndent()
+                    )
+                }
+            }
+        } ?: throw MainCommonServiceImplFingerprint.exception
     }
 }
