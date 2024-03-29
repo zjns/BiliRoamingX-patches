cc05ca7ea53aca1efd67d28d03c5cb7ae2b10c0b
Kofua <1638183271zjn@gmail.com>
2024-03-30 00:54:00 +0800

    fix(bilibili): crash

--- src/main/kotlin/app/revanced/patches/bilibili/misc/integrations/patch/IntegrationsPatch.kt
+++ src/main/kotlin/app/revanced/patches/bilibili/misc/integrations/patch/IntegrationsPatch.kt
@@ -3,10 +3,15 @@ package app.revanced.patches.bilibili.misc.integrations.patch
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod.Companion.toMutable
 import app.revanced.patches.bilibili.misc.integrations.fingerprints.InitFingerprint
+import app.revanced.patches.bilibili.utils.Method
+import app.revanced.patches.bilibili.utils.MethodImplementation
 import app.revanced.patches.shared.misc.integrations.BaseIntegrationsPatch
+import com.android.tools.smali.dexlib2.AccessFlags
 
 @Patch(
     name = "Integrations",
@@ -29,13 +34,25 @@ object IntegrationsPatch : BaseIntegrationsPatch(
             invoke-static {p0}, Lapp/revanced/bilibili/patches/main/ApplicationDelegate;->onCreate(Landroid/app/Application;)V
         """.trimIndent()
         )
-        result.mutableClass.methods.first {
-            it.name == "attachBaseContext" && it.parameterTypes == listOf("Landroid/content/Context;")
-        }.addInstructions(
-            0, """
-            invoke-static {p1}, Lapp/revanced/bilibili/patches/main/ApplicationDelegate;->attachBaseContext(Landroid/content/Context;)Landroid/content/Context;
-            move-result-object p1
-        """.trimIndent()
-        )
+        val clazz = result.mutableClass
+        if (clazz.methods.any { it.name == "getResources" && it.parameters.isEmpty() })
+            throw PatchException("Application#getResources method existed, need rewrite logic!")
+        Method(
+            definingClass = clazz.type,
+            name = "getResources",
+            returnType = "Landroid/content/res/Resources;",
+            accessFlags = AccessFlags.PUBLIC.value,
+            implementation = MethodImplementation(registerCount = 2)
+        ).toMutable().apply {
+            addInstructions(
+                0, """
+                invoke-super {p0}, ${clazz.superclass}->getResources()Landroid/content/res/Resources;
+                move-result-object v0
+                invoke-static {v0}, Lapp/revanced/bilibili/patches/main/ApplicationDelegate;->getResources(Landroid/content/res/Resources;)Landroid/content/res/Resources;
+                move-result-object v0
+                return-object v0
+            """.trimIndent()
+            )
+        }.let { clazz.methods.add(it) }
     }
 }
